de30eb5d9d541e5cf817b2f1b7a5f16b7c4c765d
jlabedo <1191198+jlabedo@users.noreply.github.com>
2025-10-06 12:00:33 +0200

    string conversion

--- packages/Ecotone/tests/Messaging/Unit/Conversion/ReferenceServiceConverterBuilderTest.php
+++ packages/Ecotone/tests/Messaging/Unit/Conversion/ReferenceServiceConverterBuilderTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Test\Ecotone\Messaging\Unit\Conversion;
 
+use Ecotone\Lite\EcotoneLite;
+use Ecotone\Messaging\Attribute\Converter;
 use Ecotone\Messaging\Handler\ServiceActivator\ServiceActivatorBuilder;
 use Ecotone\Messaging\Support\InvalidArgumentException;
+use Ecotone\Modelling\Attribute\CommandHandler;
+use Ecotone\Modelling\Attribute\EventHandler;
 use Ecotone\Test\ComponentTestBuilder;
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 use stdClass;
 use Test\Ecotone\Messaging\Fixture\Annotation\Converter\ExampleConverterService;
 use Test\Ecotone\Messaging\Fixture\Annotation\Converter\ExampleIncorrectConverterService;
@@ -96,5 +102,40 @@ class ReferenceServiceConverterBuilderTest extends TestCase
                     ->withPassThroughMessageOnVoidInterface(true)
             )
             ->build();
+    }
+
+    public function test_not_calling_converter_if_source_and_target_are_compatible()
+    {
+        $converter = new class {
+            public int $wasCalled = 0;
+
+            #[Converter]
+            public function toUuid(string $id): UuidInterface
+            {
+                $this->wasCalled++;
+                return Uuid::fromString($id);
+            }
+
+            #[Converter]
+            public function fromUUid(Uuid $id): string
+            {
+                $this->wasCalled++;
+                return $id->toString();
+            }
+        };
+        $handler = new class {
+            #[CommandHandler('handleString')]
+            public function handle(string $id): void
+            {
+                Uuid::fromString($id);
+            }
+        };
+
+        EcotoneLite::bootstrapFlowTesting(
+            [$converter::class, $handler::class],
+            [$converter, $handler],
+        )->sendCommandWithRoutingKey('handleString', Uuid::uuid4());
+
+        $this->assertEquals(0, $converter->wasCalled);
     }
 }
